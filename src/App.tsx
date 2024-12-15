e2c936633a937f17515cee4b6af7617803e410af
EbrahimRamadan1 <clinton.hema@gmail.com>
2024-12-15 17:46:18 +0200

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <main className="py-10 h-screen space-y-5 overflow-y-auto">
-      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <h1 className="font-bold text-3xl text-center">Your Tasks</h1>
       <TodoSummary
         todos={todos}
         deletedAllCompleted={deleteAllCompletedTodos}
